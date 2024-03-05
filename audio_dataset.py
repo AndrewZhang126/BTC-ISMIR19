15523c5ddc3b87c8fd5975a79a6e82381104f43d
Alex Zhang <7alexzhang7@gmail.com>
2024-03-05 03:15:27 -0800

    finetune code

--- audio_dataset.py
+++ audio_dataset.py
@@ -239,7 +239,7 @@ def _collate_fn(batch):
         feature = sample['feature']
         aug_feature = sample['aug_feature']
         chord = sample['chord']
-        diff = np.diff(chord, axis=0).astype(np.bool)
+        diff = np.diff(chord, axis=0).astype(bool)
         idx = np.insert(diff, 0, True, axis=0)
         chord_lens[i] = np.sum(idx).item(0)
         chords.extend(chord)
